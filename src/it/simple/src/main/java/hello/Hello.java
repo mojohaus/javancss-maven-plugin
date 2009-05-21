9a2cdfc6b8ddd27a35683c68381abd6530c55743
Hervé Boutemy <hboutemy@apache.org>
2009-05-21 13:21:53 +0000

    [MJNCSS-16] added a check in IT for annotations support
    git-svn-id: file:///home/tiste/MOJOHAUS-TO-GIT/SVN-MOJO-WIP/trunk/mojo/javancss-maven-plugin@9756 52ab4f32-60fc-0310-b215-8acea882cd1b

--- src/it/simple/src/main/java/hello/Hello.java
+++ src/it/simple/src/main/java/hello/Hello.java
@@ -4,6 +4,9 @@ public class Hello
 {
     public String hello( String name )
     {
+        @SuppressWarnings("unused")
+        int notUsedVariable = 2;
+
         final String myName = name == null ? "world" : name;
         return "Hello " + myName + "!";
     }
