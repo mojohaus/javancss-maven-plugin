53946799508eb806997a2b9735e1eef0f097f2f1
Hervé Boutemy <hboutemy@apache.org>
2009-05-21 13:28:54 +0000

    [MJNCSS-11] added a test in IT to check that a variable named "enum" does not make the plugin crash
    git-svn-id: file:///home/tiste/MOJOHAUS-TO-GIT/SVN-MOJO-WIP/trunk/mojo/javancss-maven-plugin@9757 52ab4f32-60fc-0310-b215-8acea882cd1b

--- src/it/simple/src/main/java/hello/Hello.java
+++ src/it/simple/src/main/java/hello/Hello.java
@@ -1,11 +1,15 @@
 package hello;
 
+import java.util.Vector;
+
 public class Hello
 {
     public String hello( String name )
     {
         @SuppressWarnings("unused")
         int notUsedVariable = 2;
+
+        Enumeration enum = new Vector().elements();
 
         final String myName = name == null ? "world" : name;
         return "Hello " + myName + "!";
