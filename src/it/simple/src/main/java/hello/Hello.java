6b32d8ee95bcccf81269ddb9bb2cadb7bbd706af
Hervé Boutemy <hboutemy@apache.org>
2009-05-23 11:23:38 +0000

    [MJNCSS-33] check that generics parsing does not fail any more
    git-svn-id: file:///home/tiste/MOJOHAUS-TO-GIT/SVN-MOJO-WIP/trunk/mojo/javancss-maven-plugin@9773 52ab4f32-60fc-0310-b215-8acea882cd1b

--- src/it/simple/src/main/java/hello/Hello.java
+++ src/it/simple/src/main/java/hello/Hello.java
@@ -7,6 +7,8 @@ public class Hello
     @Deprecated
     public static final String SYS_PROPERTY_LOADBALANCER = "s";
 
+    private Map<String, Enum<?>[]> enumerations = new HashMap<String, Enum<?>[]>(0);
+
     public String hello( String name )
     {
         @SuppressWarnings("unused")
