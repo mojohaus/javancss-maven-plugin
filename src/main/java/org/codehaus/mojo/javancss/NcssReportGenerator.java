b83e9dd7f31bf983bb43ff3c6c6c66298a684e36
Jean-Laurent de Morlhon <jeanlaurent@codehaus.org>
2009-06-02 15:04:27 +0000

    Javadoc, Checkstyle and Licence fix
    git-svn-id: file:///home/tiste/MOJOHAUS-TO-GIT/SVN-MOJO-WIP/trunk/mojo/javancss-maven-plugin@9906 52ab4f32-60fc-0310-b215-8acea882cd1b

--- src/main/java/org/codehaus/mojo/javancss/NcssReportGenerator.java
+++ src/main/java/org/codehaus/mojo/javancss/NcssReportGenerator.java
@@ -1,19 +1,22 @@
 package org.codehaus.mojo.javancss;
 
 /*
- * Copyright 2004-2005 The Apache Software Foundation.
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ *   http://www.apache.org/licenses/LICENSE-2.0
  *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
  */
 
 import java.util.Collections;
@@ -44,6 +47,8 @@ public class NcssReportGenerator
      *
      * @param sink the sink which will be used for reporting.
      * @param bundle the correct RessourceBundle to be used for reporting.
+     * @param log the log to output log with.
+     * @param xrefLocation the location of the xref file.
      */
     public NcssReportGenerator( Sink sink, ResourceBundle bundle, Log log, String xrefLocation )
     {
