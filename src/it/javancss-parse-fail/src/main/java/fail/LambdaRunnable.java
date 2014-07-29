06eef1780ac57460866c12a8abb481fea0780958
Hervé Boutemy <hboutemy@apache.org>
2014-07-29 22:22:00 +0000

    fixed IT
    git-svn-id: file:///home/tiste/MOJOHAUS-TO-GIT/SVN-MOJO-WIP/trunk/mojo/javancss-maven-plugin@19896 52ab4f32-60fc-0310-b215-8acea882cd1b

--- src/it/javancss-parse-fail/src/main/java/fail/LambdaRunnable.java
+++ src/it/javancss-parse-fail/src/main/java/fail/LambdaRunnable.java
@@ -4,7 +4,7 @@ package fail;
  * As of javancss-maven-plugin 2.1/JavaNCSS 33.54, this Java 8 new Lambda syntax is not supported.
  * lambda code sample taken from http://www.oracle.com/webfolder/technetwork/tutorials/obe/java/Lambda-QuickStart/index.html
  */
-public class RunnableTest
+public class LambdaRunnable
 {
   public static void main( String[] args )
   {
